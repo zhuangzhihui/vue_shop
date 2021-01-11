1b0e8ee9f05438e8c19a80b3336add970ef2c13f
黄志辉 <1367428189@qq.com>
2021-01-11 21:39:23 +0800

    添加商品基本页面布局

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+      <!-- 提示信息 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
+      <!-- 步骤条 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+      <!-- tab栏 -->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-position="top" label-width="100px">
+        <el-tabs v-model="activeIndex" :tab-position="'left'">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Add',
+    data() {
+      return {
+        // 步骤条验证规则
+        activeIndex: '0',
+        // 添加商品的表单数据对象
+        addForm: {
+          goods_name: '',
+          goods_price: 0,
+          goods_weight: 0,
+          goods_number: 0
+        },
+        // 添加商品的表单验证规则
+        addFormRules: {
+          goods_name: [
+            { required: true, message: '请输入商品名称', trigger: 'blur' }
+          ],
+          goods_price: [
+            { required: true, message: '请输入商品价格', trigger: 'blur' }
+          ],
+          goods_weight: [
+            { required: true, message: '请输入商品重量', trigger: 'blur' }
+          ],
+          goods_number: [
+            { required: true, message: '请输入商品数量', trigger: 'blur' }
+          ]
+        },
+        // 商品分类数据列表
+        cateList: []
+      }
+    },
+    created() {
+      this.getCateList()
+    },
+    methods: {
+      async getCateList() {
+        const { data: res } = await this.$http.get('categories')
+        if (res.meta.status !== 200) {
+          this.$message.error('获取分类数据列表失败！')
+        }
+        this.cateList = res.data
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/components/goods/List.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getGoodsList">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="goAddPage">添加商品</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- table 表格区域 -->
+      <el-table :data="goodsList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="goods_name" label="商品名称"></el-table-column>
+        <el-table-column prop="goods_price" label="商品价格（元）" width="110px"></el-table-column>
+        <el-table-column prop="goods_weight" label="商品重量" width="70px"></el-table-column>
+        <el-table-column prop="add_time" label="创建时间" width="140px">
+          <template slot-scope="scope">
+            {{scope.row.add_time | dateFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="150px">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini">编辑</el-button>
+            <el-button type="danger" size="mini" @click="removeById(scope.row.goods_id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total" background>
+      </el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+    data() {
+      return {
+        // 查询参数对象
+        queryInfo: {
+          query: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        // 商品数据
+        goodsList: [],
+        // 总数据条数
+        total: 0
+      }
+    },
+    created() {
+      this.getGoodsList()
+    },
+    methods: {
+      // 根据分页获取对应的商品列表
+      async getGoodsList() {
+        const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
+        if (res.meta.status !== 200) {
+          this.$message.error('获取商品列表失败！')
+        }
+        this.goodsList = res.data.goods
+        this.total = res.data.total
+      },
+      // 分页每页显示数据条数改变触发
+      handleSizeChange(newSize) {
+        this.queryInfo.pagesize = newSize
+        this.getGoodsList()
+      },
+      // 分页页码值发生改变触发
+      handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        this.getGoodsList()
+      },
+      // 根据ID删除对应商品数据
+      async removeById(id) {
+        const confirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+        if (confirmResult !== 'confirm') {
+          return this.$message.info('已取消删除！')
+        }
+        const { data: res } = await this.$http.delete(`goods/${id}`)
+        if (res.meta.status !== 200) {
+          this.$message.error('删除商品失败！')
+        }
+        this.$message.success('删除商品成功！')
+        this.getGoodsList()
+      },
+      // 跳转到添加商品页面
+      goAddPage() {
+        this.$router.push('/goods/add')
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
