47ffcb98c7429e464ecd30150f12005ef26596bd
黄志辉 <1367428189@qq.com>
2021-01-16 20:30:20 +0800

    订单列表基本处理

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -15,6 +15,59 @@
           </el-input>
         </el-col>
       </el-row>
+
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{scope.row.create_time | dateFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showBox"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+      <!-- 修改地址对话框 -->
+      <el-dialog
+        title="修改地址" :visible.sync="addressVisible" width="50%">
+        <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
+          <el-form-item label="省市区/县" prop="address1">
+            <el-input v-model="addressForm.address1"></el-input>
+          </el-form-item>
+          <el-form-item label="详细地址" prop="address2">
+            <el-input v-model="addressForm.address2"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
     </el-card>
   </div>
 </template>
@@ -30,7 +83,20 @@
           pagesize: 10
         },
         total: 0,
-        orderList: []
+        orderList: [],
+        addressVisible: false,
+        addressForm: {
+          address1: [],
+          address2: ''
+        },
+        addressFormRules: {
+          address1: [
+            { required: true, message: '请选择省市区/县', trigger: 'blur' }
+          ],
+          address2: [
+            { required: true, message: '请填写详细地址', trigger: 'blur' }
+          ]
+        }
       }
     },
     created() {
@@ -42,9 +108,20 @@
         if (res.meta.status !== 200) {
           return this.$message.error('获取订单列表数据失败！')
         }
-        console.log(res.data)
         this.total = res.data.total
         this.orderList = res.data.goods
+      },
+      handleSizeChange(newSize) {
+        this.queryInfo.pagesize = newSize
+        this.getOrderList()
+      },
+      handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        this.getOrderList()
+      },
+      // 展示修改地址的对话框
+      showBox() {
+        this.addressVisible = true
       }
     }
   }
