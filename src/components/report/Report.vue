0356da681f7c3f51c0f3f4d731038a8bf19d79a9
黄志辉 <1367428189@qq.com>
2021-01-18 21:24:03 +0800

    update

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -16,7 +16,7 @@
 
 <script>
   // 1.导入 echarts
-  // import echarts from 'echarts'
+  import echarts from 'echarts'
 
   export default {
     name: 'Report',
@@ -28,31 +28,31 @@
     // echarts 必须在页面元素加载完成后才能执行
     mounted() {
       // 3.基于准备好的dom，初始化echarts实例
-      // var myChart = echarts.init(document.getElementById('main'))
+      var myChart = echarts.init(document.getElementById('main'))
 
       // 4.准备数据和配置项
       // 指定图表的配置项和数据
-      // var option = {
-      //   title: {
-      //     text: 'ECharts 入门示例'
-      //   },
-      //   tooltip: {},
-      //   legend: {
-      //     data: ['销量']
-      //   },
-      //   xAxis: {
-      //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      //   },
-      //   yAxis: {},
-      //   series: [{
-      //     name: '销量',
-      //     type: 'bar',
-      //     data: [5, 20, 36, 10, 10, 20]
-      //   }]
-      // }
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      }
 
       // 5.展示数据
-      // myChart.setOption(option)
+      myChart.setOption(option)
     },
     methods: {}
   }
