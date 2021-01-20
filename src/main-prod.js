fd0fcfc54d9445124865caa175c775014cfa2fc9
黄志辉 <1367428189@qq.com>
2021-01-20 21:48:06 +0800

    开发模式与发布模式

---
+++ src/main-prod.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 导入 NProgress 进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// axios 请求拦截器添加token，保证拥有获取数据的权限（除了登录外的其它接口，均需要用户登录才有获取数据的权限）
+// 在 request 拦截器中添加进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  // 为请求头对象，添加Token验证的Authorization字段，字段的值为token
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 固定写法  在最后必须return config
+  return config
+})
+// 在 response 拦截器中 隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的 组件
+Vue.use(VueQuillEditor)
+
+// 自定义时间格式化过滤器
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
